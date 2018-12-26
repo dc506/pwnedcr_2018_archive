3fd022caa0b558e50302f570d7a4006bb4c99d09
cedriczirtacic <cicatriz.r00t@gmail.com>
2018-12-26 09:58:26 -0600

    Adding Randall Barnett data

---
+++ slides/Pentesting_en_Sistemas_Autonomos_con_Python-Randall_Barnett/SNMP02.py
@@ -0,0 +1,19 @@
+from pysnmp.hlapi import *
+
+errorIndication, errorStatus, errorIndex, varBinds = next(
+    getCmd(SnmpEngine(),
+           CommunityData('public'),
+           UdpTransportTarget(('demo.snmplabs.com', 161)),
+           ContextData(),
+           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),
+           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0')))
+)
+
+if errorIndication:
+    print(errorIndication)
+elif errorStatus:
+    print('%s at %s' % (errorStatus.prettyPrint(),
+                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+else:
+    for varBind in varBinds:
+        print(' = '.join([x.prettyPrint() for x in varBind]))
